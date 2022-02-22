4b9f627a352a8a6596894c8fab26c40ce087d70c
gpatarin <gael.patarin@outlook.com>
2022-02-22 10:02:00 +0100

    Initial commit

---
+++ src/main/java/com/ynov/logicieladaptatif/LogicielAdaptatifApplication.java
@@ -0,0 +1,13 @@
+package com.ynov.logicieladaptatif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogicielAdaptatifApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogicielAdaptatifApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ynov/logicieladaptatif/LogicielAdaptatifApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ynov.logicieladaptatif;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogicielAdaptatifApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
